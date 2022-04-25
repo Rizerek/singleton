00863c15440626454ea48e1e86ce83a3ad212376
Uczen <antek>
2022-04-25 14:08:33 +0200

    FirstCommit1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	singleton singleton1 = singleton.getInstance("numer1");
+    singleton singleton2 = singleton.getInstance("numer2");
+    singleton singleton3 = singleton.getInstance("numer3");
+    System.out.println(singleton3.value);
     }
 }
